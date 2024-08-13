c84ae648e6023b0f65b1099226b3f3a2c86eb454
soul <1041707577@qq.com>
2024-08-13 09:35:28 +0800

    feat: 一次处理完所有任务

--- tts_utils.py
+++ tts_utils.py
@@ -6,7 +6,7 @@ chat.load(compile=False)  # compile=True for better performance
 
 def infer_audio(texts):
     wavs = []
-    for i in range(0, len(texts), 5):  # Process texts in batches of 5
+    for i in range(0, len(texts), 99): 
         batch = texts[i:i+5]
         batch_wavs = chat.infer(batch)
         wavs.extend(batch_wavs)
