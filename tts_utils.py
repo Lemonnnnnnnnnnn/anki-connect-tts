1aafa20898f26446f6476a3cb57bce77c67ca004
soul <1041707577@qq.com>
2024-08-13 10:49:46 +0800

    feat: specific language with English

--- tts_utils.py
+++ tts_utils.py
@@ -2,12 +2,8 @@ import ChatTTS
 
 # 初始化 ChatTTS
 chat = ChatTTS.Chat()
-chat.load(compile=False)  # compile=True for better performance
+chat.load(compile=True)  # compile=True for better performance
 
 def infer_audio(texts):
-    wavs = []
-    for i in range(0, len(texts), 99): 
-        batch = texts[i:i+5]
-        batch_wavs = chat.infer(batch)
-        wavs.extend(batch_wavs)
+    wavs = chat.infer(text= texts, lang='en')
     return wavs
