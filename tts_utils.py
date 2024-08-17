27b78855a901228c52e11493b622ac346548201c
velvet crow <lemon.asd99@gmail.com>
2024-08-17 16:42:54 +0800

    coqui-ai-tts support

--- tts_utils.py
+++ tts_utils.py
@@ -1,13 +1,22 @@
-import ChatTTS
+import torch
+from TTS.api import TTS
 
-# 初始化 ChatTTS
-chat = ChatTTS.Chat()
-chat.load(compile=False)  # compile=True for better performance
+MODEL_NAME = "tts_models/en/ljspeech/tacotron2-DDC"  # 选择你需要的模型
 
-def infer_audio(texts):
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Init TTS
+tts = TTS(model_name=MODEL_NAME).to(device)
+
+def infer_audio(texts: list):
+    # Run TTS
+    # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+    # Text to speech list of amplitude values as output
     wavs = []
-    for i in range(0, len(texts), 5):  # Process texts in batches of 5
-        batch = texts[i:i+5]
-        batch_wavs = chat.infer(batch)
-        wavs.extend(batch_wavs)
+    for text in texts:
+        wav = tts.tts(text=text)
+        wavs.append(wav)
+
     return wavs
+
